c5860bdaf06f28fbf596a97e22ddd0eec696ac01
Robert Xu <robxu9@gmail.com>
2012-08-02 20:33:26 -0400

    Bit of cleanup.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/net/teamio/server/utils/cartload/CLlistener.java
+++ src/main/java/net/teamio/server/utils/cartload/CLlistener.java
@@ -1,10 +1,9 @@
 package net.teamio.server.utils.cartload;
 
-import org.bukkit.Chunk;
+import java.util.Collection;
+
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
@@ -16,54 +15,107 @@ import org.bukkit.event.vehicle.VehicleCreateEvent;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.util.Vector;
 
 public class CLlistener implements Listener {
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onVehicleCreate(VehicleCreateEvent e){
-		Vehicle v = e.getVehicle();
-		if (v instanceof Minecart){
-			if (CartLoad.config.getProperty("minecarts_dont_slow_down","false").equalsIgnoreCase("true"))
-				((Minecart)v).setSlowWhenEmpty(false);
-		}
-	}
-
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onVehicleMove(VehicleMoveEvent event) {
-		if (!CartLoad.config.getProperty("load_chunks_on_move", "true")
-				.equalsIgnoreCase("false")) {
-			Vehicle vehicle = event.getVehicle();
-			Location location = vehicle.getLocation();
-			Block block = location.getBlock();
-			Chunk chunk = block.getChunk();
-
-			int x = chunk.getX();
-			int z = chunk.getZ();
-
-			World world = chunk.getWorld();
-
-			int radius = Math.abs(Integer.parseInt(CartLoad.config.getProperty(
-					"radius_of_loaded_chunks", "3")));
-
-			for (int i = -radius; i <= radius; i++) {
-				for (int j = -radius; j <= radius; j++) {
-					if (world.isChunkLoaded(x + i, j + z))
-						continue;
-					world.loadChunk(x + i, z + j);
+	public void onWorldLoad(WorldLoadEvent e) {
+		Collection<Minecart> entities = e.getWorld().getEntitiesByClass(Minecart.class);
+		for (Minecart minecart : entities) {
+			World world = minecart.getWorld();
+			Location pos = minecart.getLocation();
+			
+			int posX = pos.getBlockX();
+			int posZ = pos.getBlockZ();
+			
+			int crossPosX = posX % 16; // Chunk positions (0-15)
+			int crossPosZ = posZ % 16; // ^
+			
+			if (crossPosX <= 3) {
+				world.getChunkAt(posX - 5, posZ).load();
+				
+				if (crossPosZ <= 3) {
+					world.getChunkAt(posX - 5, posZ - 5).load();
+				} else if (crossPosZ >= 12) { // 15 - 3 = 12
+					world.getChunkAt(posX - 5, posZ + 5).load();
 				}
+			} else if (crossPosX >= 12) { // 15 - 3 = 12
+				world.getChunkAt(posX + 5, posZ).load();
+				
+				if (crossPosZ <= 3) {
+					world.getChunkAt(posX + 5, posZ - 5).load();
+				} else if (crossPosZ >= 12) { // 15 - 3 = 12
+					world.getChunkAt(posX + 5, posZ + 5).load();
+				}
+			}
+			
+			if (crossPosZ <= 3) {
+				world.getChunkAt(posX, posZ - 5).load();
+			} else if (crossPosZ >= 12) { // 15 - 3 = 12
+				world.getChunkAt(posX, posZ + 5).load();
 			}
 		}
 	}
-
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onVehicleCreate(VehicleCreateEvent e) {
+		Vehicle v = e.getVehicle();
+		if (v instanceof Minecart) {
+			if (CartLoad.config.getProperty("minecarts_dont_slow_down", "false").equalsIgnoreCase("true"))
+				((Minecart) v).setSlowWhenEmpty(false);
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onVehicleMove(VehicleMoveEvent event) {
+		if (!CartLoad.config.getProperty("load_chunks_on_move", "true").equalsIgnoreCase("false")) {
+			Vehicle vehicle = event.getVehicle();
+			if (!(vehicle instanceof Minecart))
+				return;
+			Minecart minecart = (Minecart) vehicle;
+			World world = minecart.getWorld();
+			Location pos = minecart.getLocation();
+			
+			int posX = pos.getBlockX();
+			int posZ = pos.getBlockZ();
+			
+			int crossPosX = posX % 16; // Chunk positions (0-15)
+			int crossPosZ = posZ % 16; // ^
+			
+			if (crossPosX <= 3) {
+				world.getChunkAt(posX - 5, posZ).load();
+				
+				if (crossPosZ <= 3) {
+					world.getChunkAt(posX - 5, posZ - 5).load();
+				} else if (crossPosZ >= 12) { // 15 - 3 = 12
+					world.getChunkAt(posX - 5, posZ + 5).load();
+				}
+			} else if (crossPosX >= 12) { // 15 - 3 = 12
+				world.getChunkAt(posX + 5, posZ).load();
+				
+				if (crossPosZ <= 3) {
+					world.getChunkAt(posX + 5, posZ - 5).load();
+				} else if (crossPosZ >= 12) { // 15 - 3 = 12
+					world.getChunkAt(posX + 5, posZ + 5).load();
+				}
+			}
+			
+			if (crossPosZ <= 3) {
+				world.getChunkAt(posX, posZ - 5).load();
+			} else if (crossPosZ >= 12) { // 15 - 3 = 12
+				world.getChunkAt(posX, posZ + 5).load();
+			}
+		}
+	}
+	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onVehicleEntityCollision(VehicleEntityCollisionEvent event) {
 		boolean instanceofplayer = event.getEntity() instanceof Player;
-		boolean ignoreplayers = Boolean.parseBoolean(CartLoad.config
-				.getProperty("minecarts_ignore_players"));
-		boolean ignoreentities = Boolean.parseBoolean(CartLoad.config
-				.getProperty("minecarts_ignore_entities"));
-
+		boolean ignoreplayers = Boolean.parseBoolean(CartLoad.config.getProperty("minecarts_ignore_players"));
+		boolean ignoreentities = Boolean.parseBoolean(CartLoad.config.getProperty("minecarts_ignore_entities"));
+		
 		if ((instanceofplayer) && (ignoreplayers)) {
 			event.setCancelled(true);
 			event.setCollisionCancelled(true);
@@ -71,10 +123,8 @@ public class CLlistener implements Listener {
 			event.setCancelled(true);
 			event.setCollisionCancelled(true);
 		}
-
-		if (((event.getEntity() instanceof Player))
-				&& (CartLoad.config.getProperty("minecarts_run_over_players",
-						"false").equalsIgnoreCase("true"))) {
+		
+		if (((event.getEntity() instanceof Player)) && (CartLoad.config.getProperty("minecarts_run_over_players", "false").equalsIgnoreCase("true"))) {
 			if (event.getVehicle().getVelocity().length() > 0.08D) {
 				Vector velocity = event.getVehicle().getVelocity();
 				Location location = event.getVehicle().getLocation();
@@ -84,14 +134,10 @@ public class CLlistener implements Listener {
 				velocity.setX(dx);
 				velocity.setZ(dz);
 				velocity.setY(1);
-
+				
 				event.getEntity().setVelocity(velocity);
 			}
-		} else if (((event.getEntity() instanceof LivingEntity))
-				&& (!(event.getEntity() instanceof Player))
-				&& (CartLoad.config.getProperty("minecarts_run_over_mobs",
-						"false").equalsIgnoreCase("true"))
-				&& (event.getVehicle().getVelocity().length() > 0.08D)) {
+		} else if (((event.getEntity() instanceof LivingEntity)) && (!(event.getEntity() instanceof Player)) && (CartLoad.config.getProperty("minecarts_run_over_mobs", "false").equalsIgnoreCase("true")) && (event.getVehicle().getVelocity().length() > 0.08D)) {
 			Vector velocity = event.getVehicle().getVelocity();
 			Location location = event.getVehicle().getLocation();
 			Location loc2 = event.getEntity().getLocation();
@@ -104,35 +150,62 @@ public class CLlistener implements Listener {
 			event.getEntity().setVelocity(velocity);
 		}
 	}
-
+	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onChunkUnload(ChunkUnloadEvent event) {
-		if (CartLoad.config
-				.getProperty("keep_stationary_carts_loaded", "true")
-				.equalsIgnoreCase("true")) {
-			Chunk chunk = event.getChunk();
-			int x = chunk.getX();
-			int z = chunk.getZ();
-
-			World world = chunk.getWorld();
-
-			int radius = Math.abs(Integer.parseInt(CartLoad.config.getProperty(
-					"radius_of_loaded_chunks", "3")));
-
-			for (int i = -radius; i <= radius; i++) {
-				for (int j = -radius; j <= radius; j++) {
-					Chunk testchunk = world.getChunkAt(x + i, z + j);
-					Entity[] entities = testchunk.getEntities();
-
-					for (Entity entity : entities){
-						if ((entity instanceof Minecart)){
+		if (CartLoad.config.getProperty("keep_stationary_carts_loaded", "true").equalsIgnoreCase("true")) {
+			Collection<Minecart> entities = event.getWorld().getEntitiesByClass(Minecart.class);
+			for (Minecart minecart : entities) {
+				World world = minecart.getWorld();
+				Location pos = minecart.getLocation();
+				
+				int posX = pos.getBlockX();
+				int posZ = pos.getBlockZ();
+				
+				int crossPosX = posX % 16; // Chunk positions (0-15)
+				int crossPosZ = posZ % 16; // ^
+				
+				if (crossPosX <= 3) {
+					if (world.getChunkAt(posX - 5, posZ).equals(event.getChunk())) {
+						event.setCancelled(true);
+					}
+					
+					if (crossPosZ <= 3) {
+						if (world.getChunkAt(posX - 5, posZ - 5).equals(event.getChunk())) {
 							event.setCancelled(true);
-							return;
 						}
+					} else if (crossPosZ >= 12) { // 15 - 3 = 12
+						if (world.getChunkAt(posX - 5, posZ + 5).equals(event.getChunk())) {
+							event.setCancelled(true);
+						}
+					}
+				} else if (crossPosX >= 12) { // 15 - 3 = 12
+					if (world.getChunkAt(posX + 5, posZ).equals(event.getChunk())) {
+						event.setCancelled(true);
+					}
+					
+					if (crossPosZ <= 3) {
+						if (world.getChunkAt(posX + 5, posZ - 5).equals(event.getChunk())) {
+							event.setCancelled(true);
+						}
+					} else if (crossPosZ >= 12) { // 15 - 3 = 12
+						if (world.getChunkAt(posX + 5, posZ + 5).equals(event.getChunk())) {
+							event.setCancelled(true);
+						}
+					}
+				}
+				
+				if (crossPosZ <= 3) {
+					if (world.getChunkAt(posX, posZ - 5).equals(event.getChunk())) {
+						event.setCancelled(true);
+					}
+				} else if (crossPosZ >= 12) { // 15 - 3 = 12
+					if (world.getChunkAt(posX, posZ + 5).equals(event.getChunk())) {
+						event.setCancelled(true);
 					}
 				}
 			}
 		}
 	}
-
+	
 }
--- src/main/java/net/teamio/server/utils/cartload/CartLoad.java
+++ src/main/java/net/teamio/server/utils/cartload/CartLoad.java
@@ -5,8 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -14,85 +13,95 @@ import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CartLoad extends JavaPlugin {
-
+	
 	public static Properties config = new Properties();
 	public static File configFile = new File("CartLoad.config");
 	public static Properties oldconfig = new Properties();
-
+	
 	Logger log = Logger.getLogger("Minecraft");
-
+	
 	public void onEnable() {
-		ConfigLoad();
-
-		this.log.info("CartLoad v" + getDescription().getVersion()
-				+ " has been loaded.");
-
+		configLoad();
+		
+		this.log.info("CartLoad v" + getDescription().getVersion() + " has been loaded.");
+		
 		PluginManager pm = getServer().getPluginManager();
-
+		
 		pm.registerEvents(new CLlistener(), this);
-
-		if (!config.getProperty("load_all_carts_on_startup").equalsIgnoreCase(
-				"false")) {
-			List<World> worlds = getServer().getWorlds();
-
-			for (World world : worlds) {
-				List<Entity> entities = world.getEntities();
-				for (Entity entity : entities){
-					if ((entity instanceof Minecart)) {
-						int x = entity.getLocation().getBlockX();
-						int z = entity.getLocation().getBlockZ();
-						int radius = Math.abs(Integer.parseInt(config
-								.getProperty("radius_of_loaded_chunks", "3")));
-
-						for (int i = -radius; i <= radius; i++) {
-							for (int j = -radius; j <= radius; j++) {
-								if (world.isChunkLoaded(x + i, j + z))
-									continue;
-								world.loadChunk(x + i, z + j);
-							}
-						}
-					}
-				}
-			}
-		}
+		
+		// if
+		// (!config.getProperty("load_all_carts_on_startup").equalsIgnoreCase(
+		// "false")) {
+		// List<World> worlds = getServer().getWorlds();
+		//
+		// for (World world : worlds) {
+		// List<Entity> entities = world.getEntities();
+		// for (Entity entity : entities) {
+		// if ((entity instanceof Minecart)) {
+		// int x = entity.getLocation().getBlockX();
+		// int z = entity.getLocation().getBlockZ();
+		// int radius = Math.abs(Integer.parseInt(config
+		// .getProperty("radius_of_loaded_chunks", "3")));
+		//
+		// for (int i = -radius; i <= radius; i++) {
+		// for (int j = -radius; j <= radius; j++) {
+		// if (world.isChunkLoaded(x + i, j + z))
+		// continue;
+		// world.loadChunk(x + i, z + j);
+		// }
+		// }
+		// }
+		// }
+		// }
+		// }
 	}
 	
-	
-	/* (non-Javadoc)
-	 * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender
+	 * , org.bukkit.command.Command, java.lang.String, java.lang.String[])
 	 */
 	@Override
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-		if (command.getName().equalsIgnoreCase("countminecarts")){
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("countminecarts")) {
 			int totalcount = 0;
-			for (World w : getServer().getWorlds()){
-				totalcount+=w.getEntitiesByClass(Minecart.class).size();
+			for (World w : getServer().getWorlds()) {
+				totalcount += w.getEntitiesByClass(Minecart.class).size();
 			}
-			sender.sendMessage(ChatColor.YELLOW+"[CartLoad] There are "+totalcount+" minecarts on the server.");
+			sender.sendMessage(ChatColor.YELLOW + "[CartLoad] There are " + totalcount + " minecarts on the server.");
 			return true;
+		} else if (command.getName().equalsIgnoreCase("rmminecarts")) {
+			int totalcount = 0;
+			for (World w : getServer().getWorlds()) {
+				Collection<Minecart> entities = w.getEntitiesByClass(Minecart.class);
+				totalcount += entities.size();
+				for (Minecart m : entities) {
+					m.remove();
+				}
+			}
+			sender.sendMessage(ChatColor.YELLOW + "[CartLoad] There were " + totalcount + " minecarts removed.");
 		}
 		return false;
 	}
-
-	private void ConfigLoad() {
+	
+	private void configLoad() {
 		if (!configFile.exists()) {
-			ConfigCreate(false);
+			configUpdate(false);
 			this.log.info("[CartLoad] Configuration file does not exist! Creating one now...");
 		} else {
 			try {
 				FileInputStream in = new FileInputStream(configFile);
 				oldconfig.load(in);
 				in.close();
-				if (!oldconfig.getProperty("version").equals(
-						getDescription().getVersion())) {
-					ConfigCreate(true);
+				if (!oldconfig.getProperty("version").equals(getDescription().getVersion())) {
+					configUpdate(true);
 					this.log.info("[CartLoad] Configuration file out of date! Updating...");
 				} else {
 					config = oldconfig;
@@ -104,8 +113,8 @@ public class CartLoad extends JavaPlugin {
 			}
 		}
 	}
-
-	private void ConfigCreate(boolean update) {
+	
+	private void configUpdate(boolean update) {
 		if (!update) {
 			try {
 				configFile.createNewFile();
@@ -113,17 +122,17 @@ public class CartLoad extends JavaPlugin {
 				e.printStackTrace();
 			}
 		}
-
+		
 		config.put("version", getDescription().getVersion());
-		ConfigPropertyStore("load_chunks_on_move", "true", update);
-		ConfigPropertyStore("keep_stationary_carts_loaded", "true", update);
-		ConfigPropertyStore("radius_of_loaded_chunks", "3", update);
-		ConfigPropertyStore("load_all_carts_on_startup", "true", update);
-		ConfigPropertyStore("minecarts_ignore_entities", "false", update);
-		ConfigPropertyStore("minecarts_run_over_mobs", "false", update);
-		ConfigPropertyStore("minecarts_ignore_players", "false", update);
-		ConfigPropertyStore("minecarts_run_over_players", "false", update);
-		ConfigPropertyStore("minecarts_dont_slow_down","false",update);
+		configStore("load_chunks_on_move", "true", update);
+		configStore("keep_stationary_carts_loaded", "true", update);
+		configStore("radius_of_loaded_chunks", "3", update);
+		configStore("load_all_carts_on_startup", "true", update);
+		configStore("minecarts_ignore_entities", "false", update);
+		configStore("minecarts_run_over_mobs", "false", update);
+		configStore("minecarts_ignore_players", "false", update);
+		configStore("minecarts_run_over_players", "false", update);
+		configStore("minecarts_dont_slow_down", "false", update);
 		try {
 			FileOutputStream out = new FileOutputStream(configFile);
 			config.store(out, null);
@@ -134,15 +143,14 @@ public class CartLoad extends JavaPlugin {
 			e.printStackTrace();
 		}
 	}
-
-	private void ConfigPropertyStore(String key, String defaultvalue,
-			boolean update) {
+	
+	private void configStore(String key, String defaultvalue, boolean update) {
 		if ((update) && (oldconfig.contains(key)))
 			config.put(key, oldconfig.get(key));
 		else
 			config.put(key, defaultvalue);
 	}
-
+	
 	public void onDisable() {
 		this.log.info("CartLoad has been disabled - minecarts will now unload normally.");
 	}
